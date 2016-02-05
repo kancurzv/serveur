0294ac683202c76ecbee7f40324b0ac6be9162da
valentin KANCURZEWSKI <v.kancurzewski@etudiant.univ-lille1.fr>
2016-02-05 09:41:12 +0100

    Correction makefile

--- webserver/main.c
+++ webserver/main.c
@@ -1,12 +1,22 @@
 # include <stdio.h>
 # include <string.h>
-int main (int argc, char **argv)
-{
+
+int main (int argc, char **argv){
 /* Arnold Robbins in the LJ of February ’95 , describing RCS */
-if(argc > 1 && strcmp (argv [1] , " - advice") == 0){
-	printf("Don't Panic !\n");
-	return 42;
+	if(argc > 1 && strcmp (argv [1] , " - advice") == 0){
+		printf("Don't Panic !\n");
+		return 42;
+	}
+	printf("Need an advice ?\n");
+
+	int socket_serveur;
+	socket_serveur = socket ( AF_INET , SOCK_STREAM , 0);
+	if(socket_serveur == -1){
+		perror ( " socket_serveur " );
+		/* traitement de l ’ erreur */
+	}
+/* Utilisation de la socket serveur */
+	return 0;
 }
-printf("Need an advice ?\n");
-return 0;
-}
+
+
