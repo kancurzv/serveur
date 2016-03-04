cab23765ecf2abc063729b1cdf1ce05ac8ab9421
Quentin SPINNEWYN <quentin.spinnewyn@outlook.fr>
2016-03-04 10:05:41 +0100

    SOREUSEADDR

--- webserver/socket.c
+++ webserver/socket.c
@@ -9,6 +9,7 @@ int creer_serveur(int port){
 
 	int socket_serveur ;
 	socket_serveur = socket(AF_INET,SOCK_STREAM,0);
+	
 	if(socket_serveur == -1){
 		perror(" socket_serveur ");
 	/* traitement de l ’ erreur */
@@ -20,6 +21,12 @@ int creer_serveur(int port){
 	saddr . sin_port = htons (port); /* Port d ’ écoute */
 	saddr . sin_addr . s_addr = INADDR_ANY ; /* écoute sur toutes lesinterfaces*/
 
+	int optval = 1;
+	if (setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1){
+	  perror("Can not set SO_REUSEADDR");
+	}
+	
+	
 	if(bind(socket_serveur,(struct sockaddr*)&saddr,sizeof(saddr)) == -1){
 		perror ( " bind socker_serveur " );
 		/* traitement de l ’ erreur */
@@ -40,6 +47,8 @@ int creer_serveur(int port){
 	const char*message_bienvenue="Bonjour,bienvenue sur mon serveur \n";
 	write(socket_client ,message_bienvenue,strlen(message_bienvenue));
 
-
 	return 0;
 }
+
+
+
